f3d70c6bc8270b4037907ec0d527c321e8a5e39f
komronium <komronium@gmail.com>
2025-11-25 21:45:10 +0500

    product/cateories/ -> /categories/

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from payment.views import PaymeCallBackAPIView, ClickWebhookAPIView
-
+from product.views import CategoryListAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('orders/', include('order.urls')),
     path('products/', include('product.urls')),
+    path('categories/', CategoryListAPIView.as_view()),
     path('payment/payme/update/', PaymeCallBackAPIView.as_view()),
     path("payment/click/update/", ClickWebhookAPIView.as_view()),
 ]
--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from product.views import CategoryListAPIView, ProductDetailAPIView, ProductListAPIView
+from product.views import ProductDetailAPIView, ProductListAPIView
 
 
 urlpatterns = [
     path('', ProductListAPIView.as_view(), name='product-list'),
     path('<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
-    path('categories/', CategoryListAPIView.as_view(), name='category-list'),
 ]
